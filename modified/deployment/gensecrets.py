e5cd77e00c19d7227c9e6f332d6272f62f5b591e
Andrew Langan <alangan444@icloud.com>
2024-02-09 12:50:26 -0500

    ✨ Fixed Python secrets scripts

--- modified/deployment/gensecrets.py
+++ modified/deployment/gensecrets.py
@@ -3,7 +3,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
 
-output = open("deployment/global_secrets.h", "wt", encoding="utf-8")
+output = open("global_secrets.h", "wt", encoding="utf-8")
 
 
 def gen_boot_keypair_A() -> tuple[bytes, bytes]:
@@ -64,7 +64,7 @@ def generate_replacement_keypair() -> tuple[bytes, bytes]:
 replacement_pub, replacement_priv = generate_replacement_keypair()
 replacement_pub = replacement_pub.hex()
 replacement_priv = replacement_priv.hex()
-print(f"Replacement Params:\n{replacement_pub=}\n{replacement_priv=}")
+print(f"Replacement Params:\n{replacement_pub=}\n{replacement_priv=}\n")
 
 keypair_A_priv, keypair_A_pub = gen_boot_keypair_A()
 keypair_B_priv, keypair_B_pub = gen_boot_keypair_B()
@@ -73,15 +73,15 @@ keypair_D_priv, keypair_D_pub = gen_boot_keypair_D()
 
 keypair_A_priv = keypair_A_priv.hex()
 keypair_A_pub = keypair_A_pub.hex()
-print(f"Keypair A:\n{keypair_A_priv=}\n{keypair_A_pub=}")
+print(f"Keypair A:\n{keypair_A_priv=}\n{keypair_A_pub=}\n")
 
 keypair_B_priv = keypair_B_priv.hex()
 keypair_B_pub = keypair_B_pub.hex()
-print(f"Keypair B:\n{keypair_B_priv=}\n{keypair_B_pub=}")
+print(f"Keypair B:\n{keypair_B_priv=}\n{keypair_B_pub=}\n")
 
 keypair_C_priv = keypair_C_priv.hex()
 keypair_C_pub = keypair_C_pub.hex()
-print(f"Keypair C:\n{keypair_C_priv=}\n{keypair_C_pub=}")
+print(f"Keypair C:\n{keypair_C_priv=}\n{keypair_C_pub=}\n")
 
 keypair_D_priv = keypair_D_priv.hex()
 keypair_D_pub = keypair_D_pub.hex()
