f5f7daa4059ef2ea2b5f99cc1d22a2dbc5fac92b
Andrew Langan <alangan444@icloud.com>
2024-02-14 12:42:47 -0500

    ⏮️ REmoved ectf_params from repo

--- modified/application_processor/inc/ectf_params.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ECTF_PARAMS__
-#define __ECTF_PARAMS__
-#define AP_PIN "123456"
-#define AP_TOKEN "1234567812345678"
-#define COMPONENT_IDS 2
-#define COMPONENT_CNT 2
-#define AP_BOOT_MSG "hi"
-#endif
