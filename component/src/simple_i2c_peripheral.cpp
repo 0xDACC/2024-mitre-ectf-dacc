887afcbf5ea9138b6efd669164cf1e35b80e3727
Andrew Langan <alangan444@icloud.com>
2024-03-07 11:24:19 -0500

    ✨ Improve I2C implementation

--- component/src/simple_i2c_peripheral.cpp
+++ component/src/simple_i2c_peripheral.cpp
@@ -36,8 +36,6 @@ error_t i2c_simple_peripheral_init(const uint8_t addr, const i2c_cb_t cb) {
     MXC_I2C_SetFrequency(MXC_I2C1, I2C_FREQ);
     MXC_I2C_SetClockStretching(MXC_I2C1, 1);
     MXC_I2C_DisablePreload(MXC_I2C1);
-    MXC_I2C_SetRXThreshold(MXC_I2C1, 0);
-    MXC_I2C_SetTXThreshold(MXC_I2C1, 8);
 
     MXC_I2C_EnableInt(MXC_I2C1, MXC_F_I2C_INTFL0_RD_ADDR_MATCH, 0);
     MXC_I2C_EnableInt(MXC_I2C1, MXC_F_I2C_INTFL0_WR_ADDR_MATCH, 0);
@@ -104,30 +102,25 @@ void i2c_simple_isr() {
             txcnt = 0;
             if (call_processing_callback() != error_t::SUCCESS) {
                 printf("Failed to call processing callback\n");
-                return;
             }
             printf("CALLBACK SUCCESS\n");
         }
 
         const uint8_t available = MXC_I2C_GetTXFIFOAvailable(MXC_I2C1);
-        if (txcnt >= txsize) {
-            // Send null bytes cause of some weird bug
+        if (txcnt >= txsize || txsize == 0) {
+            // Send null bytes cause of some weird bug?
             uint8_t buf[8] = {};
             MXC_I2C_WriteTXFIFO(MXC_I2C1, buf, 8);
-        } else if (available > (txsize - txcnt) && txsize > 0) {
+        } else if (available > (txsize - txcnt)) {
             // Send the remaining bytes
             txcnt +=
                 MXC_I2C_WriteTXFIFO(MXC_I2C1, txbuf + txcnt, txsize - txcnt);
-        } else if (txsize > 0) {
+        } else {
             // Send the available bytes
             txcnt += MXC_I2C_WriteTXFIFO(MXC_I2C1, txbuf + txcnt, available);
-        } else {
-            // Send null bytes cause of some weird bug
-            uint8_t buf[8] = {};
-            MXC_I2C_WriteTXFIFO(MXC_I2C1, buf, 8);
         }
 
-        if (txcnt >= txsize && txsize > 0) {
+        if (txcnt >= txsize) {
             MXC_I2C_DisableInt(MXC_I2C1, MXC_F_I2C_INTEN0_TX_THD, 0);
         }
     }
@@ -142,6 +135,8 @@ void i2c_simple_isr() {
 
         if ((flags & MXC_F_I2C_INTFL0_TX_LOCKOUT) != 0) {
             MXC_I2C_ClearFlags(MXC_I2C1, MXC_F_I2C_INTFL0_TX_LOCKOUT, 0);
+            txsize = 0;
+            txcnt = 0;
         }
 
         MXC_I2C_EnableInt(MXC_I2C1, MXC_F_I2C_INTEN0_TX_THD, 0);
@@ -173,6 +168,10 @@ void i2c_simple_isr() {
         } else {
             // Read the available bytes
             rxcnt += MXC_I2C_ReadRXFIFO(MXC_I2C1, rxbuf + rxcnt, available);
+        }
+
+        if (rxcnt >= bufsize) {
+            MXC_I2C_DisableInt(MXC_I2C1, MXC_F_I2C_INTEN0_RX_THD, 0);
         }
 
         MXC_I2C_ClearFlags(MXC_I2C1, MXC_F_I2C_INTFL0_RX_THD, 0);
